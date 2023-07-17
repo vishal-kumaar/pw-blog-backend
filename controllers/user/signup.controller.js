ccc42120d4b8b5475100b2b9e5d6ac0e6db4276a
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-18 00:40:40 +0530

    Create signup user controller

--- controllers/user/signup.controller.js
+++ controllers/user/signup.controller.js
@@ -2,6 +2,14 @@ import asyncHandler from "../../utils/asyncHandler";
 import CustomError from "../../utils/CustomError";
 import User from "../../schemas/user.schema";
 
+/********************************************************
+ * @SIGNUP
+ * @route https://localhost:4000/api/auth/signup
+ * @description User signup controller for creating a new user
+ * @parameters name, email, password
+ * @return User Object
+ *********************************************************/
+
 const signUp = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -19,16 +27,18 @@ const signUp = asyncHandler(async (req, res) => {
     name,
     email,
     password,
-  })
+  });
 
   const token = user.getJwtToken();
   await user.save();
   user.password = undefined;
-  
+
   res.status(200).json({
     success: true,
     message: "User created successfully",
     token,
-    user
-  })
+    user,
+  });
 });
+
+export default signUp;
