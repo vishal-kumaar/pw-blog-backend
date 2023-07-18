8e2fecb7d81ac14244eb578e148e25fd0e0459e3
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-18 18:45:25 +0530

    Add port and mongoDbUri in config

--- config/config.js
+++ config/config.js
@@ -5,6 +5,8 @@ const config = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiry: process.env.JWT_EXPIRY,
   allowedOrigins: process.env.ALLOWED_ORIGINS.split(","),
+  mongoDbUri: process.env.MONGODB_URI,
+  port: process.env.PORT,
 };
 
 export default config;
