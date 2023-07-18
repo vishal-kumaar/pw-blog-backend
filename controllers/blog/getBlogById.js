b84a7bab31e2b3010bdcdff1bfd5545c9c271200
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-18 11:40:52 +0530

    Create get blog by id controller

---
+++ controllers/blog/getBlogById.js
@@ -0,0 +1,28 @@
+import asyncHanlder from "../../utils/asyncHandler.js";
+import CustomError from "../../utils/CustomError.js";
+import Blog from "../../schemas/blog.schema.js";
+
+/***************************************************
+ * @GET_BLOG_BY_ID
+ * @method GET
+ * @route /api/blog/:blogId
+ * @description Get blog by Id controller to get specific blog by blog id
+ * @parameters none
+ * @return Blog Object
+ ************************************************/
+
+const getBlogById = asyncHanlder(async (req, res) => {
+  const { blogId } = req.params;
+  const blog = await Blog.findById(blogId);
+
+  if (!blog) {
+    throw new CustomError("Blog not found", 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    blog,
+  });
+});
+
+export default getBlogById;
