2c67ed2a26a7a0004e6ea5dc2780a9be117ff697
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-18 18:00:24 +0530

    Create a config for cors

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 const config = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiry: process.env.JWT_EXPIRY,
+  allowedOrigins: process.env.ALLOWED_ORIGINS.split(","),
 };
 
 export default config;
