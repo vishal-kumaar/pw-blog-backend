761571157b9e2275444fb005982f001d49f34e84
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 19:26:37 +0530

    Add session expired message in IsLoggedIn middleware

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -16,7 +16,7 @@ const isLoggedIn = asyncHandler(async (req, _res, next) => {
     req.user = await User.findById(decodedJwtPayload._id, "name email");
     next();
   } catch (error) {
-    throw new CustomError("Not authorized to access this route", 401);
+    throw new CustomError("Session expired! Login to continue", 401);
   }
 });
 
