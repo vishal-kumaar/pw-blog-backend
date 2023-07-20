8e42e4a5bd6583cb6e6768c0c55aaea8ce588ba7
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-20 16:11:07 +0530

    Remove isLoggedOut middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,10 +1,8 @@
 import isLoggedIn from "./isLoggedIn.js";
-import isLoggedOut from "./isLoggedOut.js";
 import isAuthor from "./isAuthor.js";
 
 const middlewares = {
   isLoggedIn,
-  isLoggedOut,
   isAuthor,
 };
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,9 @@
 import express from "express";
-import middlewares from "../middlewares/index.js";
 import controllers from "../controllers/index.js";
 
 const router = express.Router();
 
-router.post("/user/register", middlewares.isLoggedOut, controllers.register);
-router.post("/user/login", middlewares.isLoggedOut, controllers.login);
+router.post("/user/register", controllers.register);
+router.post("/user/login", controllers.login);
 
 export default router;
